7ee143a39f7e4710a74578e20f3daf7a69a91b53
kuonggaa <96900121+kuonggaa@users.noreply.github.com>
2024-05-03 10:56:16 +0700

    Update playwright.spec.js

--- playwright.spec.js
+++ playwright.spec.js
@@ -1,33 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('watch first video on YouTube', async ({ page }) => {
-  // Mở trang YouTube
-  await page.goto('https://www.youtube.com/');
-  
-  // Nhập từ khóa vào ô tìm kiếm trên YouTube
-  const youtubeSearchInput = await page.waitForSelector('input#search');
-  await youtubeSearchInput.type('Playwright tutorial');
-  await youtubeSearchInput.press('Enter');
-
-  // Chờ kết quả tìm kiếm trên YouTube
-  await page.waitForNavigation();
-
-  // Click vào video đầu tiên từ kết quả tìm kiếm
-  const firstVideoLink = await page.waitForSelector('a#video-title');
-  await firstVideoLink.click();
-
-  // Chờ trang video load
-  await page.waitForNavigation();
-
-  // Thực hiện hơn 10 thao tác bổ sung trên trang video
-  // Ví dụ: tìm và click vào nút "Like", "Subscribe", "Share", vv.
-  await page.waitForSelector('button#like-button');
-  await page.click('button#like-button');
-  await page.waitForSelector('button#subscribe-button');
-  await page.click('button#subscribe-button');
-  await page.waitForSelector('button#share-button');
-  await page.click('button#share-button');
-
-  // Chờ một thời gian để xem video hoặc thực hiện các thao tác khác
-  await page.waitForTimeout(5000);
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.goto('https://www.google.com/search?q=wiki&oq=wiki&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQyOThqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8');
+  await page.getByRole('link', { name: 'Wikipedia, bách khoa toàn th' }).click();
+  await page.getByPlaceholder('Tìm kiếm trên Wikipedia').click();
+  await page.getByPlaceholder('Tìm kiếm trên Wikipedia').fill('open ai');
+  await page.getByPlaceholder('Tìm kiếm trên Wikipedia').press('Enter');
+  await page.getByRole('link', { name: 'Huấn luyện' }).click();
+  await page.getByRole('link', { name: 'Tính năng' }).click();
+  await page.getByRole('link', { name: 'Dịch vụ' }).click();
+  await page.getByRole('link', { name: 'Đón nhận, chỉ trích, và những' }).click();
+  await page.getByRole('link', { name: 'Phản ứng tích cực' }).click();
+  await page.getByRole('link', { name: 'Phản ứng tiêu cực' }).click();
+  await page.getByRole('link', { name: 'Tác động đến nền giáo dục' }).click();
 });
